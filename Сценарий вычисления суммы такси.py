c2b73731c8eccd7e15d8ab18519e07a0a236753f
G0gss <123230512+G0gss@users.noreply.github.com>
2023-06-17 09:34:50 +0300

    Сценарий вычисления суммы такси.py

--- Сценарий вычисления суммы такси.py
+++ Сценарий вычисления суммы такси.py
@@ -1,30 +1,27 @@
 import datetime # Добавил модуль datatime для дальнейшего его использования в деконаторе
 
-def taxi_payment(distance): #Создал функцию taxi_payment которая принимает расстояние в киллометрах
-    # Конвертируем расстояние в километрах в расстояние в метрах
+def taxi_payment(distance): 
+   
     distance_meters = distance * 1000
-    # Считаем количество блоков по 140 метров (это нужно для того чтобы в легче было расчитывать стоймость поездки, так как за каждые 140 метров к стоймости добавляется 0.25$ )
+   
     num_blocks = distance_meters // 140
-    # Считаем итоговую стоимость
+
     total_payment = 4 + num_blocks * 0.25
     return total_payment
 
-# Создал декоратор для логирования вызовов функции
+
 def log_calls(func):
     def wrapper(*args, **kwargs):
-        # Получаем текущую дату и время с помощбю комманд модуля datetime
+       
         now = datetime.datetime.now()
-        # Выводим информацию о вызове функции
         print(f"Функция {func.__name__}, текущая дата и время {now}")
         print(f"Аргумент: {args}, {kwargs}")
-        # Вызываем функцию и сохраняем результат
         result = func(*args, **kwargs)
-        # Выводим результат
         print(f"Результат: {result}")
         return result
     return wrapper
 
-# Добавил декоратор к функции taxi_payment
+# декоратор к функции taxi_payment
 @log_calls
 def taxi_payment_decorated(distance):
     return taxi_payment(distance)
